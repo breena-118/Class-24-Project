098344114042eb14ce1e2bfdb6fd3787c55127b6
breena-118 <80144818+breena-118@users.noreply.github.com>
2021-06-04 22:22:05 +0530

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,31 @@
+class Iron{
+	constructor(x,y,r)
+	{
+	// assign options to the rubber ball
+	    var options ={
+		'restitution' : 0.8,
+		'friction' : 3,
+		'density' : 30
+	    }
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var ironpos=this.body.position;		
+			push()
+			translate(ironpos.x, ironpos.y);
+			rectMode(CENTER)
+			strokeWeight(5);
+			stroke("black");
+			fill("brown");
+			//draw the ellipse here to display the rubber ball
+            
+			pop()
+	}
+
+}
